b01e2f427c2a4e74abe091f2e7d934acdfd6dd6e
Fifi Arisandi <fifi_arisandi@yahoo.com>
2021-12-12 23:23:44 +0200

    revise Navbar

--- save-miko-reactjs/src/App.js
+++ save-miko-reactjs/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layouts/Header";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
--- save-miko-reactjs/src/components/layouts/Navbar.js
+++ save-miko-reactjs/src/components/layouts/Navbar.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../../logosaveMiko.png'
+import React from "react";
+import styled from "styled-components";
+import logo from "../../logosaveMiko.png";
 
 const Navbar = () => {
     return (
         <NavbarContainer>
-            <nav className="navbar navbar-expand-lg navbar-light px-5 py-0">
+            <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container">
+
                 <a className="navbar-brand" href="#"> 
-                <img style={{width: "50px"}} src={logo} alt="logo" />
+                <img className="logo" src={logo} alt="logo" />
                 </a>
                 <button 
                     className="navbar-toggler" 
@@ -17,22 +19,26 @@ const Navbar = () => {
                     aria-controls="navbarSupportedContent" 
                     aria-expanded="false" 
                     aria-label="Toggle navigation"
-                >
+                    >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav ms-auto">
                         <li className="nav-item active">
                             <a className="nav-link" href="#">
-                                Home 
+                                Home <span className="sr-only"></span>
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Sponsor a Dog</a>
                         </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Us</a>
+                        </li>
                     </ul>
                 </div>
+            </div>
             </nav>
         </NavbarContainer>
     )
