f2736b09480201a4ea49ca20a53f35136401e090
Fifi Arisandi <fifi_arisandi@yahoo.com>
2021-12-13 15:22:39 +0200

    aboutUs

--- save-miko-reactjs/src/App.js
+++ save-miko-reactjs/src/App.js
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layouts/Header";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
+import AboutUs from "./components/layouts/AboutUs";
 
 function App() {
   return (
     <div className="App"> 
     <Header />
     <Navbar />
+    <AboutUs />
     <Footer />
     </div>
   );
---
+++ save-miko-reactjs/src/components/layouts/AboutUs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import team from "../../teamwork.png"
+
+const AboutUs = () => {
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-lg-6 col-sm-12">
+                    <div className="photoWrap mb-5">
+                        <img className="team-img" src={team} alt="the team.."/>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-sm-12">
+                    <h2>about us</h2>
+                    <p>
+                    SaveMiko! was founded in Tallinn, Estonia by a group of animal lovers, who are excited about helping unfortunate dogs. 
+                    The name is inspired by Miko, a dog who was neglected by his owners for years, before he was finally rescued.
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default AboutUs
