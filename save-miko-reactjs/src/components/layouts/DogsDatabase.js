82c2a847524f32941d8ab4ab6b4c36a75f41712c
Fifi Arisandi <fifi_arisandi@yahoo.com>
2021-12-16 00:12:31 +0200

    tidying up

--- save-miko-reactjs/src/components/layouts/DogsDatabase.js
+++ save-miko-reactjs/src/components/layouts/DogsDatabase.js
@@ -11,38 +11,8 @@ import "react-popupbox/dist/react-popupbox.css";
 import { Link } from "react-scroll";
 
 
-//import Popup from "reactjs-popup";
-//import "reactjs-popup/dist/index.css";
-
-
 const DogsDatabase = () => {
-    //Lucky
-    //const OpenpopupLucky = () => {
-    //    const content = (
-    //    <>
-    //    <img className="popupbox-img" src={lucky} alt="Hi, I'm Lucky.."></img>
-    //    <p>
-    //        I have such a fun time playing scent games with my carers and sniffing around the fields to see what I can find. 
-    //        I love a good splash about in the local river too!
-
-    //        I can be very protective over my things and can get very anxious around people I don't know. 
-    //        However, I'm super lucky that my carers here understand me, know all my quirks and are helping me build my confidence around people.
-    //    </p>
-    //    <b>Sponsor me!</b> <a className="hyperlink" onClick={() => window.open("https://www.facebook.com/")}>https://www.facebook.com/</a>
-    //    </>
-    //    )
-        //PopupboxManager.open({ content })
-    //}
-
-    //const popupConfigLucky = {
-    //    titleBar: {
-    //        enable: true,
-    //        text: "Sponsor Lucky"
-    //    },
-    //    fadeIn: true,
-    //    fadeInSpeed: 500
-    //}
-
+    
     return (
         <div id="sponsor" className="database-wrapper">
             <div className="container">
@@ -59,8 +29,6 @@ const DogsDatabase = () => {
                         <Link smooth={true} to="signup" className="nav-link" href="#">
                             <button type="button" className="sponsor-btn"> Sponsor me! </button>
                         </Link>
-                        {/* <div className="overflow"></div>
-                        <FontAwesomeIcon className="database-icon" icon={faSearchPlus} />  */}
                     </div>
                 {/* - */}
                     <div className="dog-img-box">
@@ -75,8 +43,6 @@ const DogsDatabase = () => {
                         <Link smooth={true} to="signup" className="nav-link" href="#">
                             <button type="button" className="sponsor-btn"> Sponsor me! </button>
                         </Link>
-                        {/* <div className="overflow"></div>
-                        <FontAwesomeIcon className="database-icon" icon={faSearchPlus} /> */}
                     </div>
                 {/* - */}
                     <div className="dog-img-box"> 
@@ -87,12 +53,12 @@ const DogsDatabase = () => {
 
                         <p className="dog-text">I was a stray before I came here. 
                         I get really anxious and wary around new people and dogs so finding a home that suits me is tricky.
+                        I love being here, everyone is nice to me. But, I hope I will get my forever family one day.
                         </p>
                         <Link smooth={true} to="signup" className="nav-link" href="#">
                             <button type="button" className="sponsor-btn"> Sponsor me! </button>
                         </Link>
-                        {/* <div className="overflow"></div>
-                        <FontAwesomeIcon className="database-icon" icon={faSearchPlus} /> */}
+                        {/*  */}
                     </div>
                 </div>
             </div>
